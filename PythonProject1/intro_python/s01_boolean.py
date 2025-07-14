fd4c198c5d0e70757d6de88cfb039965ff0604fd
Adailton <paazadailton@gmail.com>
2025-07-14 07:18:50 -0300

    new creations

---
+++ PythonProject1/intro_python/s01_boolean.py
@@ -0,0 +1,35 @@
+age = 12
+print(age)
+
+print(f'minha idade é {age}')
+
+my_number = 5
+user_number = int(input("Enter a number: "))
+matches = my_number == user_number
+print(f"You got it right: {matches}.")
+
+
+age = 25
+result = age >  26 and age < 30
+print(result)
+
+default_greeting = "There"
+name = input("Enter your name(opcional) ")
+user_name = name or default_greeting
+print(f"Hello, {user_name}")
+
+
+#Tuple
+
+friends = ("Rolf", "Bob", "Anne")
+friends = friends + ("Jen",)
+print(friends)
+
+
+
+
+
+
+
+
+
---
+++ PythonProject1/intro_python/s02_lists.py
@@ -0,0 +1,26 @@
+friends = ["Rolf", "Anne"]
+friends.append("Adam")
+print(friends)
+friends.remove("Rolf")
+print(friends)
+
+print(len(friends))
+
+friends2 = [["Rolf", 24], ["Bob", 30], ["Anne", 32]]
+print((friends2[2][1]))
+print(friends2[0][0])
+friends2.append(["Adam", 46])
+print((friends2))
+friends2.remove(["Bob", 30])
+print(friends2)
+
+friends3 = [
+    ["Rolf", 24],
+    ["Bob", 30],
+    ["Anne", 32],
+    ["Jen", 45]
+]
+
+
+
+
