04dde20157ef6893d17c2b23875fdb0cf3cecff3
Adailton <paazadailton@gmail.com>
2026-01-12 23:33:11 -0300

    new python code

---
+++ files_project/milestone2/utils/database.py
@@ -0,0 +1,17 @@
+books = []
+
+
+def add_book(name, author):
+    books.append({"name": name, "author": author, "read": False})
+
+def get_all_books():
+    return books
+
+def mark_read_book(name):
+    for book in books:
+        if book['name'] == name:
+            book['read'] = True
+
+def delete_book(name):
+    global books
+    books = [book for book in books if book['name'] != name]
