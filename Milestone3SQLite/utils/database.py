7eb46ccd398c42bb7203c65d7353939885e167d7
Adailton Paz <140769695+PazAdailton@users.noreply.github.com>
2026-01-15 10:30:37 -0300

    new python code

--- Milestone3SQLite/utils/database.py
+++ Milestone3SQLite/utils/database.py
@@ -1,58 +1,57 @@
-import sqlite3
+from typing import List, Dict, Union
+from .database_connection import DatabaseConnection
 """
 json file
 
-json.load TRANSFORMA JSON EM OBJETO PYTHON
+json.load TRANSFORMA JSON EM OBJETO PYTHON              /// typing hinting -> List[Dict]: -> None: say what is the return of the function
 
 json.dump TRANSFORMA OBJETO PYTHON EM JSON
 
+
 """
+Book = Dict[str, Union[str, int]]
 
-def create_book_table():
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+def create_book_table() -> None:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('create table IF NOT EXISTS books(name text primary key, author text, read integer)')
-
-    connection.commit()
-    connection.close()
-
-
-def add_book(name, author):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-
-    cursor.execute('insert into books values (?, ?, 0)', (name, author))
-
-    connection.commit()
-    connection.close()
-
-def get_all_books():
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-
-    cursor.execute('select * from books')
-    books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
-    connection.close()
-
-    return books
-
-def _save_all_books(books):
-    with open(books_file, 'w') as file:
-        json.dump(books, file, indent=2)
+        cursor.execute("""
+            create table IF NOT EXISTS books
+                (name text primary key, 
+                author text, 
+                read integer)
+        """)
 
 
-def mark_book_as_read(name):
-    books = get_all_books()
-    for book in books:
-        if book['name'] == name:
-            book['read'] = True
-    _save_all_books(books)
+def add_book(name: str, author: str) -> None:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('insert into books values (?, ?, 0)', (name, author))
 
 
-def delete_book(name):
-    books = get_all_books()
-    new_books = [book for book in books if book['name'] != name]
-    _save_all_books(new_books)
+
+def get_all_books() -> List[Book]:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('select * from books')
+        books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
+        return books
+
+def mark_book_as_read(name: str) -> None:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('update books set read=1 WHERE name = ?', (name,))
+
+
+def delete_book(name: str) -> None:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('delete from books where name = ?', (name,))
+
+
 
 
