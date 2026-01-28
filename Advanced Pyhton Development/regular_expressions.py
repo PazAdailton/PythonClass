03e63638cd10b536e6e0fd3cbc692ff2895bb87d
Adailton <paazadailton@gmail.com>
2026-01-27 22:08:29 -0300

    new python code

---
+++ Advanced Pyhton Development/regular_expressions.py
@@ -0,0 +1,16 @@
+"""
+Four most important components
+
+"." == anything like letters, numbers, symbols... but not newlines... OBS: Only characters
+
+"+" == one or more of
+
+"*" == zero or more of... OBS get all word
+
+"?" == zero or one of
+
+ "\" literal of symbol
+
+
+
+"""
