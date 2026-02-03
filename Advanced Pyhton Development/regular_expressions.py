1026c4202bb7720d1dc29d97a433af60a16f6d13
Adailton <paazadailton@gmail.com>
2026-02-02 23:06:00 -0300

    new python code

--- Advanced Pyhton Development/regular_expressions.py
+++ Advanced Pyhton Development/regular_expressions.py
@@ -17,9 +17,23 @@ import re
 
 
 name = 'charlie'
-email = 'jose@tecladocode.com'
+name2 = 'Jo.se'
+email = 'jose@tecladocode.me'
+any_string = """
+  charlie hello - I am Jose
+              hi - there
 
-regexer = '[a-z]'
+"""
 
-match = re.findall(regexer, name)
+
+regexer1 = '[a-z]+' #name
+regexer2 = '[a-z].*' #name
+regexer3 = '[A-z]+' #with capital letters
+regexer4 = r'[A-z\.]+' #name2
+regexer5 = r'[A-z\.]+' #usar raw string return == ['jose', 'tecladocode.com']
+regexer6 = r'[A-z\.]+@[A-z\.]+' #return ['jose@tecladocode.com']
+regexer7 = r'[A-z\.]+@[A-z]+\.' #['jose@tecladocode.']
+regexer8 = r'[A-z\.]+@[A-z]+\.com'
+regexer9 = r'[A-z\.]+@[A-z]+\.(com|me)'
+match = re.findall(regexer9, email)
 print(match)
